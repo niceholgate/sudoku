59ac7004754067b4bc9ad73b01089e2ee9b1452e
Nicholas Holgate <niceholgate@gmail.com>
2023-07-20 14:47:41 +0800

    add UtilsTests.cs

---
+++ SudokuTests/Models/UtilsTests.cs
@@ -0,0 +1,37 @@
+﻿namespace SudokuTests;
+
+using System.Linq;
+using Sudoku.Models;
+
+[TestClass]
+public class UtilsTests {
+    [TestMethod]
+    public void TestCheckGridEquivalence_True() {
+        int[,] grid1 = {
+                         { 3, 1, 6 },
+                         { 5, 2, 9 },
+                         { 4, 8, 7 } };
+
+        int[,] grid2 = {
+                         { 3, 1, 6 },
+                         { 5, 2, 9 },
+                         { 4, 8, 7 } };
+
+        Assert.IsTrue(Utils<int>.CheckGridEquivalence(grid1, grid2));
+    }
+
+    [TestMethod]
+    public void TestCheckGridEquivalence_False() {
+        int[,] grid1 = {
+                         { 3, 1, 6 },
+                         { 5, 2, 9 },
+                         { 4, 8, 7 } };
+
+        int[,] grid2 = {
+                         { 1, 1, 6 },
+                         { 5, 2, 9 },
+                         { 4, 8, 7 } };
+
+        Assert.IsFalse(Utils<int>.CheckGridEquivalence(grid1, grid2));
+    }
+}
